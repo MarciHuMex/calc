8e815e39b46e2dd0e072ad3cdcd0d6de8eefcb93
MarciHuMex <151879261+MarciHuMex@users.noreply.github.com>
2024-04-28 18:30:19 +0200

    add style to calculator

--- calc.js
+++ calc.js
@@ -17,3 +17,27 @@ function divide(a, b) {
         return a / b;
     }
 }
+
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+': 
+            return add(num1, num2);
+        case '-':
+            return substract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Invalid operator";
+    }
+}
+
+// Store the display Value
+let displayValue = '';
+
+// Function to update the display with the clicked number
