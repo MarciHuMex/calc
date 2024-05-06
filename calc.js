10926adc2e312a703f9ea5c0c2ff6eb06a98db9b
MarciHuMex <151879261+MarciHuMex@users.noreply.github.com>
2024-05-06 19:54:30 +0200

    calculator works

--- calc.js
+++ calc.js
@@ -24,6 +24,7 @@ function divide(a, b) {
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
+let displayValue = '';
 
 function operate(operator, num1, num2) {
     switch (operator) {
@@ -42,9 +43,6 @@ function operate(operator, num1, num2) {
 
 
 
-// Store the display value
-let displayValue = '';
-
 // Function to update the display with the clicked number
 function updateDisplay(number) {
     // Append the clicked number to the display value
@@ -61,6 +59,32 @@ document.querySelectorAll('.number').forEach(button => {
         // Update the display with the clicked number
         updateDisplay(number);
     });
+});
+
+document.querySelectorAll('.operator').forEach(button => {
+        button.addEventListener('click', () => {
+            operator = button.textContent;
+            firstNumber = parseFloat(displayValue);
+            updateDisplay(operator);
+        });
+});
+
+document.getElementById('equals').addEventListener('click', () => {
+    secondNumber = parseFloat(displayValue.substring(displayValue.lastIndexOf(operator) + 1));
+    const result = operate(operator, firstNumber, secondNumber);
+    document.getElementById('display').innerText  = displayValue + ' = ' + result;
+    firstNumber = '';
+    operator = '';
+    secondNumber = '';
+    displayValue = '';
+});
+
+document.getElementById('clear').addEventListener('click', () => {
+    document.getElementById('display').innerText = '';
+    firstNumber = '';
+    operator = '';
+    secondNumber = '';
+    displayValue = '';
 });
 
 });
